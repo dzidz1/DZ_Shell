57b2f3ea7a793f8f52f0ddd738934ccd944a6dca
Akaki Dzidziguri <adzid23@freeuni.edu.ge>
2025-11-13 15:49:40 +0400

    fixed minor bug in my_strncmp

--- my_helpers.c
+++ my_helpers.c
@@ -35,7 +35,7 @@ int my_strncmp(const char *str1, const char *str2, size_t n) {
   size_t i = 0;
   while (i < n && *str1 && *str2) {
     if (*str1 != *str2) {
-      return (unsigned char)str1[i] - (unsigned char)str2[i];
+      return (unsigned char)*str1 - (unsigned char)*str2;
     }
     str1++;
     str2++;
